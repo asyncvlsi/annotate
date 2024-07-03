c67f973ab9da02f19555c3fd5823fa1786cc9a21
Rajit Manohar <rajit.manohar@yale.edu>
2024-07-03 15:07:18 -0400

    read in spef and/or sdf for annotations

--- annotate_pass.cc
+++ annotate_pass.cc
@@ -21,41 +21,26 @@
 #include <act/act.h>
 #include <act/passes.h>
 #include "spef.h"
+#include "sdf.h"
 
 void annotate_pass_init (ActPass *ap)
 {
   Act *a = ap->getAct();
-  ActDynamicPass *adp = dynamic_cast<ActDynamicPass *>(ap);
-  Assert (adp, "Hmm..");
+  ActDynamicPass *dp = dynamic_cast<ActDynamicPass *>(ap);
+  Assert (dp, "Hmm..");
 
   ActPass *bp = a->pass_find ("booleanize");
 
   if (!bp) {
     bp = new ActBooleanizePass (a);
   }
-  adp->addDependency ("booleanize");
+  dp->addDependency ("booleanize");
 }
 
-/* Not defining this
-void annotate_pass_run (ActPass *ap, Process *p)
-void annotate_pass_recursive (ActPass *ap, Process *p, int mode);
-*/
 
-void *annotate_pass_proc (ActPass *ap, Process *p, int mode)
+static bool load_spef (ActDynamicPass *dp)
 {
-  char * s= NULL;
-  Spef *spf = NULL;
-  ActDynamicPass *dp = dynamic_cast<ActDynamicPass *> (ap);
-
-  if (!p || !dp->getRoot()) {
-    /* run on the top level global namespace */
-    printf ("Annotation pass must be run with a specified top-level process only.");
-    return NULL;
-  }
-
-  if (dp->getRoot() == p) {
-    /* the root process: read SPEF/SDF as specified */
-  }
+  Spef *spf;
   
   if (!dp->hasParam ("spef")) {
     char buf[1024];
@@ -82,24 +67,106 @@ void *annotate_pass_proc (ActPass *ap, Process *p, int mode)
       if (!fp) {
 	warning ("Could not open SPEF file `%s' for reading",
 		 config_get_string (buf));
-	return NULL;
+	delete spf;
+	return false;
       }
     }
     else {
       fp = fopen (buf, "r");
       if (!fp) {
-	warning ("Could not open SPEF file `%s' for reading", buf);
-	return NULL;
+	delete spf;
+	return false;
       }
     }
     spf->Read (fp);
     fclose (fp);
     dp->setParam ("spef", spf);
   }
-  else {
-    spf = (Spef *) dp->getPtrParam ("spef");
+  return true;
+}
+
+
+static bool load_sdf (ActDynamicPass *dp)
+{
+  SDF *sdf;
+  
+  if (!dp->hasParam ("sdf")) {
+    char buf[1024];
+    char *ns = NULL;
+    Process *root = dp->getRoot();
+    FILE *fp;
+    
+    Assert(root, "What?");
+    if (root->getns() && root->getns() != ActNamespace::Global()) {
+      ns = root->getns()->Name();
+    }
+    if (ns) {
+      snprintf (buf, 1024, "%s::%s.sdf", ns, root->getName());
+    }
+    else {
+      snprintf (buf, 1024, "%s.sdf", root->getName());
+    }
+    if (ns) {
+      FREE (ns);
+    }
+    sdf = new SDF (true);
+    if (config_exists (buf)) {
+      fp = fopen (config_get_string (buf), "r");
+      if (!fp) {
+	warning ("Could not open SDF file `%s' for reading",
+		 config_get_string (buf));
+	delete sdf;
+	return false;
+      }
+    }
+    else {
+      fp = fopen (buf, "r");
+      if (!fp) {
+	delete sdf;
+	return false;
+      }
+    }
+    sdf->Read (fp);
+    fclose (fp);
+    dp->setParam ("sdf", sdf);
   }
-  Assert (spf, "What?!");
+  return true;
+}  
+
+
+/* Not defining this
+void annotate_pass_run (ActPass *ap, Process *p)
+void annotate_pass_recursive (ActPass *ap, Process *p, int mode);
+*/
+
+void *annotate_pass_proc (ActPass *ap, Process *p, int mode)
+{
+  char * s= NULL;
+  Spef *spf = NULL;
+  SDF *sdf = NULL;
+  ActDynamicPass *dp = dynamic_cast<ActDynamicPass *> (ap);
+
+  if (!p || !dp->getRoot()) {
+    /* run on the top level global namespace */
+    printf ("Annotation pass must be run with a specified top-level process only.");
+    return NULL;
+  }
+
+  if (dp->getRoot() == p) {
+    if (!load_spef (dp) && !load_sdf (dp)) {
+      warning ("Annotation pass requires either SPEF or SDF file!");
+      return NULL;
+    }
+  }
+  spf = (Spef *) dp->getPtrParam ("spef");
+  sdf = (SDF *) dp->getPtrParam ("sdf");
+  if (!spf && !sdf) {
+    return NULL;
+  }
+
+  /* do something here! */
+  
+
   return NULL;
 }
 
